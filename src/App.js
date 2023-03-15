6ad2253faee093a3a07063df79f4892ce98cd0d5
francesco <fran.calabrese03@gmail.com>
2023-03-15 19:49:52 +0100

    esercizio15

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export class App extends React.Component{
    render(){
     return(
         <>
-       <Welcome name/>
+       <Welcome name="francesco" age={19}/>
       
     </>)
 }}
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export class Welcome extends React.Component{
     render(){
-    const  name ="francesco"
-   
-   return <p> "Welcome, {name}"</p>
+        
+   return (
+    <>
+    <p> "Welcome, {this.props.name}"</p>
+    <p>"your age is {this.props.age}"</p>
+    </>
+    )
 }}
