bdf46ba46cc3c615015f54eff57a71e0ce0a1a47
francesco <fran.calabrese03@gmail.com>
2023-03-15 18:29:30 +0100

    esercizio10

--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Message } from "./Message";
 
 
 export class Hello extends React.Component{
@@ -7,7 +7,7 @@ export class Hello extends React.Component{
         return(
      <>
         <h1>"Hello, World!"</h1>
-        <p>"What a beautiful day!"</p>
+        <Message />
         </>
         )
     }
