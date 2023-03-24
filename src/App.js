de9ee5620cf89c3d20b82186e1f03f8aec9b363a
francesco <fran.calabrese03@gmail.com>
2023-03-24 19:44:50 +0100

    esercizio23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 export class App extends React.Component{
    render(){
       
     return(
-        <>
-       <Welcome name="John"/>
-      
-    </>)
+        <Counter />
+    )
 }}
