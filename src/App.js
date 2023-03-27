a8e8c2f87eba04e935fb4c1ded95ee4e00b8a0be
francesco <fran.calabrese03@gmail.com>
2023-03-28 01:06:22 +0200

    esercizio27

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Counter } from "./Counter";
+import { ClickCounter } from "./ClickCounter";
 export class App extends React.Component{
    render(){
       
     return(
-        <Counter/>
+        <ClickCounter/>
     )
 }}
