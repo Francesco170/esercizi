a15adb9100971fe61eb3c405fe348fd4361a63b4
francesco <fran.calabrese03@gmail.com>
2023-03-29 02:17:04 +0200

    esercizio33

--- src/Login.js
+++ src/Login.js
@@ -1,55 +1,55 @@
-import React from "react";
-export class Login extends React.Component{
+// import React from "react";
+// export class Login extends React.Component{
   
-    state = {
-        username:"",
-        password:"",
-        remember:false,
+//     state = {
+//         username:"",
+//         password:"",
+//         remember:false,
        
-    }
+//     }
 
-    EventHandler = (event) => {
-       const value = event.target.value
+//     EventHandler = (event) => {
+//        const value = event.target.value
 
-       this.setState({
-        username : value
-       })
-    }
+//        this.setState({
+//         username : value
+//        })
+//     }
 
-    PasswordHandler = (event) => {
-        const value = event.target.value
+//     PasswordHandler = (event) => {
+//         const value = event.target.value
  
-        this.setState({
-        password : value
-        })
-     }
+//         this.setState({
+//         password : value
+//         })
+//      }
 
-    CheckHandler = (event) => {
-        const checked = event.target.checked
+//     CheckHandler = (event) => {
+//         const checked = event.target.checked
  
-        this.setState({
-       remember : checked
-        })
-     }
+//         this.setState({
+//        remember : checked
+//         })
+//      }
 
-   resetHandler=()=>{
-   this.setState({
-    username:"",
-    password:"",
-    remember:false,
-   })
-   }   
-    render(){
+//    resetHandler=()=>{
+//    this.setState({
+//     username:"",
+//     password:"",
+//     remember:false,
+//    })
+//    }   
+//     render(){
             
-    return(
-        <>
+//     return(
+//         <>
              
-            <input name="username"   onChange={this.EventHandler} value={this.state.username}/>  
-            <input name="password" type="password"  onChange={this.PasswordHandler} value={this.state.password}/> 
-            <input name="remember" type="checkbox" onChange={ this.CheckHandler} checked={this.state.remember} /> 
-              <button name="login"    disabled={!this.state.username && !this.state.password}>Login</button>    
-              <button name=" reset" onClick={this.resetHandler}> reset</button>
-        </>
-    )
-}
-}
+//             <input name="username"   onChange={this.EventHandler} value={this.state.username}/>  
+//             <input name="password" type="password"  onChange={this.PasswordHandler} value={this.state.password}/> 
+//             <input name="remember" type="checkbox" onChange={ this.CheckHandler} checked={this.state.remember} /> 
+//               <button name="login"    disabled={!this.state.username && !this.state.password}>Login</button>    
+//               <button name=" reset" onClick={this.resetHandler}> reset</button>
+//         </>
+//     )
+// }
+// }
---
+++ src/UncontrolledLogin.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+export class UncontrolledLogin extends React.Component{
+    HandleFormSubmit=(event)=>{
+        event.preventDefault()
+
+         const username= event.target.elements.username.value
+         const password= event.target.elements.password.value
+         const remember= event.target.elements.remember.checked
+
+         console.log({
+            username,password,remember
+         })
+    }
+    
+    
+    render(){
+        return(
+            <>
+            <form onSubmit={this.HandleFormSubmit} >
+            <input name="username"  />  
+            <input name="password" type="password"  /> 
+            <input name="remember" type="checkbox"  />
+
+
+            <button name="login" type="submit">Login</button>    
+            <button name=" reset" type="reset"> reset</button>
+              </form>
+            </>
+        )
+    }
+}
