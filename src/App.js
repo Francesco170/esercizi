56a1270cd26e839984a70b20f8592e18eb4dd81e
francesco <fran.calabrese03@gmail.com>
2023-03-30 01:09:06 +0200

    esercizio39

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { TodoList } from "./TodoList";
+import { Welcome } from "./Welcome";
 export class App extends React.Component{
    render(){
       
     return(
-        <TodoList name={["mario","luca", "franco", "andrea"]}/>
+        <Welcome name="francesco"/>
     )
 }}
---
+++ src/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Welcome extends React.Component{
+   render(){
+   return (
+   <div className="welcome">
+   <p> "Welcome, {this.props.name}"</p>
+   </div>)
+}}
