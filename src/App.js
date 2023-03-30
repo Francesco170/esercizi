cc1ac826b54c520412e94c2b663f8425b632b2cb
francesco <fran.calabrese03@gmail.com>
2023-03-30 15:25:27 +0200

    esercizio41

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Login } from "./Login";
+import { Container } from "./Container";
 export class App extends React.Component{
    render(){
       
     return(
-        <Login/>
+        <Container/>
     )
 }}
---
+++ src/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export class Container extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                   <p>questo è un child</p>
+            </div>
+        )
+    }
+}
